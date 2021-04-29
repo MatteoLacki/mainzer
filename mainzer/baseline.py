3ea12c5673feef7cac0a44212de4ad3ded93ac88
Michał Startek <mist@mimuw.edu.pl>
2021-04-29 14:16:58 +0200

    Implement baseline correction

---
+++ mainzer/baseline.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def strip_baseline(mzs, intensities, baseline):
+    if len(mzs) == 0:
+        return [], []
+
+    idx = 0
+    forward_idx = 0
+    lookahead = 10.0
+    dens = baseline / lookahead
+
+    res_mzs = []
+    res_int = []
+
+
+    while idx < len(mzs):
+        while forward_idx < len(mzs) and mzs[idx] + lookahead > mzs[forward_idx]:
+            forward_idx += 1
+
+        if forward_idx < len(mzs):
+            try:
+                dens = baseline * lookahead / (forward_idx - idx)
+            except ZeroDivisionError:
+                pass
+
+
+        if intensities[idx] >= dens:
+            res_mzs.append(mzs[idx])
+            res_int.append(intensities[idx])
+
+        idx += 1
+        forward_idx = max(idx, forward_idx)
+
+    return np.array(res_mzs), np.array(res_int)
+
