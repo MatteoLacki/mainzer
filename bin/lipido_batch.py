37cadbdf41ec1e6342bc2dc5b6b3b5abaa99fcc8
MatteoLacki <matteo.lacki@gmail.com>
2021-05-03 15:33:57 +0200

    renaming to lipido_batch

---
+++ bin/lipido_batch.py
@@ -0,0 +1,26 @@
+import argparse
+import glob
+import os
+import pathlib
+import sys
+
+from mainzer.lipido import lipido_main
+from mainzer.settings import Settings
+
+ap = argparse.ArgumentParser(description='Analyze batch input with lipido.',
+                             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+ap.add_argument('path_to_config_file',
+                help="Path to configuration file.",
+                type=pathlib.Path)
+ap = ap.parse_args()
+
+settings = Settings.FromTOML(ap.path_to_config_file)
+os.chdir(ap.path_to_config_file.parent)
+
+
+for path_spectrum in glob.glob(settings["path_spectrum"]):
+    path_spectrum = pathlib.Path(path_spectrum)
+    settings["path_spectrum"] = str(path_spectrum)
+    settings['output_folder'] = str(pathlib.Path(path_spectrum.stem) / "output")
+    lipido_main(settings)
+    # save settings in each folder with its run specific settings?
