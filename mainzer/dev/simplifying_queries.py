e4ccfbbcd2a37cfefcb858edbaf1edcc48d981e0
MatteoLacki <matteo.lacki@gmail.com>
2021-05-30 12:35:55 +0200

    know how to add tests and run them

---
+++ mainzer/dev/simplifying_queries.py
@@ -0,0 +1,31 @@
+%load_ext autoreload
+%autoreload 2
+
+import json
+from mainzer.read import read_spectrum
+from mainzer.signal_ops import cluster_spectrum
+from mainzer.intervals import IntervalQuery
+from mainzer.centroiding import QueryCentroids
+
+# fixing clustering
+with open("mainzer/dev/settings.json") as f:
+    settings = json.load(f)
+mz, intensity = read_spectrum(settings['path_spectrum'])
+min_intensity_threshold = 100
+
+clusters_df = cluster_spectrum(mz, intensity)
+# this is simple: filerting on `max_intensity` in `clusters_df`
+filtered_clusters_df = clusters_df[clusters_df.I_max >= min_intensity_threshold].copy()
+
+IQ = IntervalQuery(filtered_clusters_df.left_mz,
+                   filtered_clusters_df.right_mz)
+
+IQ.point_query([100, 400, 1000, 398.73])
+IQ.interval_query([100, 400, 1000, 398.73], [101, 401, 1001, 398.83])
+IQ.interval_query_tuples([(100,101), (400,401), (1000,1001), (398.73, 398.83)])
+
+filtered_centroids = QueryCentroids(filtered_clusters_df)
+
+
+
+
--- mainzer/intervals.py
+++ mainzer/intervals.py
@@ -1,8 +1,11 @@
+from collections import namedtuple
 from ncls import FNCLS
 import numpy as np
 if False: # Do NOT remove this: this is to trick pyinstaller into
           # importing the necessary packages
     import scipy.spatial.transform._rotation_groups
+
+Indices = namedtuple("IndexEdges", "query interval_db")
 
 
 class IntervalQuery(object):
@@ -17,31 +20,56 @@ class IntervalQuery(object):
     Each time, the edges of the graphs are returned iff the red and white interval intersect [sparse outcome].
     This was tested agains pandas interval indices and python intervaltree and won.
     """
-    def __init__(self, left, right, index=None):
+    def __init__(self, left, right):
         self.left  = np.array(left,  dtype=float)
         self.right = np.array(right, dtype=float)
-        index = np.arange(len(left)) if index is None else np.array(index)
-        self.fncls = FNCLS(self.left, self.right, index)
+        # ids can support indices, but this overcomplicates queries that can be simply integer based.
+        self.ids = np.arange(len(left), dtype=np.int64)
+        self.fncls = FNCLS(self.left, self.right, ids=self.ids)
 
-    def interval_query(self, left, right, index=None):
+    def interval_query(self, left, right):
+        """
+        Arguments:
+            left (list): left ends of (open) intervals.
+            right (list): right ends of (open) intervals.
+        Returns:
+            IndexEdges: named tuple defining edges between matching query intervals and those already in the interval database.
+        """
         left = np.array(left, dtype=float)
         right = np.array(right, dtype=float)
-        if index == None:
-            index = np.arange(len(left), dtype=np.int64)
-        else:
-            index = index.astype(np.int64)
-        query_idxs, db_idxs = self.fncls.all_overlaps_both(left, right, index)
-        return query_idxs.astype(np.int64), db_idxs.astype(np.int64)
+        query_idxs, db_idxs = self.fncls.all_overlaps_both(left, right, self.ids)
+        return Indices(query_idxs.astype(np.int64),
+                       db_idxs.astype(np.int64))
 
-    def point_query(self, x, index=None):
-        return self.interval_query(x,x,index)
+    def interval_query_tuples(self, query_intervals):
+        """
+        Arguments:
+            query_intervals (list): list of tuples with left and right ends of intervals.
+        Returns:
+            IndexEdges: named tuple defining edges between matching query intervals and those already in the interval database.
+        """
+        left, right = zip(*query_intervals)
+        return self.interval_query(left, right)
+
+    def point_query(self, x):
+        return self.interval_query(x,x)
 
     def __repr__(self):
         return f"[({self.left[0]:.3f}, {self.right[0]:.3f}) .. ({self.left[-1]:.3f}, {self.right[-1]:.3f})]"
 
 
+def test_IntervalQuery():
+    test_intervals = IntervalQuery([0., 2., 7.],
+                                   [1., 3., 9.])
+    index_edges = test_intervals.interval_query(left=[-2, 1.5],
+                                                right=[-1, 10.])
+    assert all(index_edges.query == [1,1]), "Wrong indices of queried intervals returned."
+    assert all(index_edges.interval_db == [1,2]), "Wrong indices of intervals in the interval database returned."
 
-# if __name__ == "__main__":
-#     test_intervals = IntervalQuery([0., 2., 7.],
-#                                    [1., 3., 9.])
-#     print(test_intervals.interval_query([1.5], [10.])
+    query_intervals = [(-2, -1), (1.5, 10)]
+    left, right = zip(*query_intervals)
+
+    index_edges_using_tuple_query = test_intervals.interval_query_tuples(query_intervals)
+    assert all(index_edges_using_tuple_query.query == index_edges.query) and \
+           all(index_edges_using_tuple_query.interval_db == index_edges.interval_db), \
+           "Results between 'interval_query' and 'interval_query_tuples' do not match."
